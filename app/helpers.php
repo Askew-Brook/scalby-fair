2d02d02a134bb4b57a20a280bd1bbdc0b229f756
Richard Askew <richard@askewbrook.com>
2026-08-03 15:35:58 +0100

    Generate Glide URLs from Statamic assets

--- app/helpers.php
+++ app/helpers.php
@@ -5,10 +5,10 @@ use Statamic\Globals\Variables;
 use Statamic\Tags\FluentTag;
 
 if (! function_exists('glide')) {
-    function glide(string $path, array $parameters = []): FluentTag
+    function glide(mixed $asset, array $parameters = []): FluentTag
     {
         return Statamic\Statamic::tag('glide')->params(array_merge([
-            'path' => $path,
+            'src' => $asset,
             'fit' => 'crop_focal',
         ], $parameters));
     }
--- resources/views/components/responsive-image.blade.php
+++ resources/views/components/responsive-image.blade.php
@@ -2,8 +2,8 @@
 @php($asset = \Statamic\View\Blade\value($asset))
 @if($asset)
     <img
-        src="{{ glide($asset->path(), ['w' => $width, 'h' => $height, 'q' => 82, 'fm' => 'webp']) }}"
-        srcset="{{ glide($asset->path(), ['w' => 640, 'h' => round(640 * $height / $width), 'q' => 80, 'fm' => 'webp']) }} 640w, {{ glide($asset->path(), ['w' => $width, 'h' => $height, 'q' => 82, 'fm' => 'webp']) }} {{ $width }}w"
+        src="{{ glide($asset, ['w' => $width, 'h' => $height, 'q' => 82, 'fm' => 'webp']) }}"
+        srcset="{{ glide($asset, ['w' => 640, 'h' => round(640 * $height / $width), 'q' => 80, 'fm' => 'webp']) }} 640w, {{ glide($asset, ['w' => $width, 'h' => $height, 'q' => 82, 'fm' => 'webp']) }} {{ $width }}w"
         sizes="{{ $sizes }}"
         width="{{ $width }}" height="{{ $height }}" alt="{{ $alt ?? ($asset->alt ?? '') }}" loading="{{ $loading }}" @if($fetchPriority) fetchpriority="{{ $fetchPriority }}" @endif decoding="async" {{ $attributes }}
     >
