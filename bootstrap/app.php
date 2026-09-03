49d5864c18bfa471a873275d119008b1d40835b2
Richard Askew <richard@askewbrook.com>
2026-09-03 17:10:36 +0100

    Chore: Implement base Stripe integration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->validateCsrfTokens(except: [
+            'stripe/webhooks/stall-bookings',
+            'stripe/webhooks/walk-bookings',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
